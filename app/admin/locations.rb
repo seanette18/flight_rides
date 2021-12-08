019b206c8c8f110a87f57523f102014f1a875041
anonymous anonymous <seanette.ting@kellogg.northwestern.edu>
2021-12-08 19:16:51 +0000

    Whitelist columns in Location for ActiveAdmin

--- app/admin/locations.rb
+++ app/admin/locations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Location do
 
+ permit_params :name_of_location, :address
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

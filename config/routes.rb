2ba6ca65608de9afad4987fe474a90d9e46cdbe2
anonymous anonymous <seanette.ting@kellogg.northwestern.edu>
2021-12-08 19:16:44 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "trips#index"
   resources :locations
   resources :trips
